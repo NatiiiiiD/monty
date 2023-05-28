9c626b19963cb7b2755d181a858a4cff486132fa
NatiiiiiD <natnaeldejene6748@gmail.com>
2023-05-28 09:09:54 -0700

    All Done

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- indicate nothing
+  *@head: indicate stack head
+  *@counter: indicate line_number
+  *Return: is no return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - this prints the top
+ * @head: indicate stack head
+ * @counter: indicate line_number
+ * Return: is no return
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
